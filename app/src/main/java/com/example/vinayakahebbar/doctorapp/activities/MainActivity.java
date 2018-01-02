229e19e5a73752dea08aebc8fb617286063c775a
SumithraM <mavillapallisumithra@gmail.com>
2018-01-02 12:36:31 +0530

    drawer_menu

--- app/src/main/java/com/example/vinayakahebbar/doctorapp/activities/MainActivity.java
+++ app/src/main/java/com/example/vinayakahebbar/doctorapp/activities/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.vinayakahebbar.doctorapp.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.vinayakahebbar.doctorapp.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
