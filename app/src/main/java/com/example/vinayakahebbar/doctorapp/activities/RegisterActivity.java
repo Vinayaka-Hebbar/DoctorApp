d5ed440b38011c7a751228da79753951c3bfb14f
vinayakahebbar <vinayakahebbar@hotmail.com>
2018-01-03 16:42:22 +0530

    changes

--- app/src/main/java/com/example/vinayakahebbar/doctorapp/activities/RegisterActivity.java
+++ app/src/main/java/com/example/vinayakahebbar/doctorapp/activities/RegisterActivity.java
@@ -4,12 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.vinayakahebbar.doctorapp.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        auth = FirebaseAuth.getInstance();
     }
 }
