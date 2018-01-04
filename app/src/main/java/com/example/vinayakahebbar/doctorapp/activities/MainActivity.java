1cc363202cdda5853905c0879e6082af99fc7511
SumithraM <mavillapallisumithra@gmail.com>
2018-01-04 11:44:51 +0530

    MainActivity

--- app/src/main/java/com/example/vinayakahebbar/doctorapp/activities/MainActivity.java
+++ app/src/main/java/com/example/vinayakahebbar/doctorapp/activities/MainActivity.java
@@ -1,15 +1,54 @@
 package com.example.vinayakahebbar.doctorapp.activities;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.graphics.drawable.DrawerArrowDrawable;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.vinayakahebbar.doctorapp.R;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private DrawerArrowDrawable arrowDrawable;
+    private NavigationView navigationView;
+    private ActionBarDrawerToggle toggle;
+    private Toolbar toolbar;
 
+    Button search;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout=(DrawerLayout)findViewById(R.id.drawer);
+        navigationView =(NavigationView)findViewById(R.id.nav_view);
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        arrowDrawable = new DrawerArrowDrawable(this);
+        toolbar.setNavigationIcon(arrowDrawable);
+
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(drawerLayout.isDrawerOpen(navigationView))
+                    drawerLayout.closeDrawer(Gravity.START);
+                else
+                    drawerLayout.openDrawer(Gravity.START);
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        return super.onOptionsItemSelected(item);
     }
 }
+
+
