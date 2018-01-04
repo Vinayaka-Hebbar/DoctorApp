beb93e6c8f4a7ea64ce3823f52a9cdc22f0ce77c
NeelimaNeelu <neelima6neelu@gmail.com>
2018-01-04 12:29:20 +0530

    forgot password

---
+++ app/src/main/java/com/example/vinayakahebbar/doctorapp/activities/ForgotActivity.java
@@ -0,0 +1,15 @@
+package com.example.vinayakahebbar.doctorapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.vinayakahebbar.doctorapp.R;
+
+public class ForgotActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot);
+    }
+}
